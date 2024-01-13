34b8c4528226abdef064c0d2a620953349f3c0b6
DivvSaxena <saxenadivv@gmail.com>
2024-01-14 01:55:30 +0530

    fixed the pic bug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
@@ -14,41 +15,67 @@ const dataInDB = ref(database , "megs")
 let textfieldEl = document.getElementById('textarea')
 let buttonEl = document.getElementById('button')
 
-let profilepic = document.getElementById('pic')
+// let profilepic = document.getElementById('pic')
 let imageField = document.getElementById('image')
 let ownerNameEl = document.getElementById('owner-name')
 let priceEl = document.getElementById('price')
 let contactEl = document.getElementById('contact')
 let megsListEl = document.getElementById('megs-list')
 
-imageField.onchange = () => {
-    profilepic.src = URL.createObjectURL(imageField.files[0])
-}
+let megItems = []
+
+
 
 
 buttonEl.addEventListener('click', () => {
     let inputValue = textfieldEl.value
     
-    push(dataInDB, inputValue)
+    //push(dataInDB, inputValue)
+
+    if(inputValue && ownerNameEl.value && priceEl.value && contactEl.value && imageField.value){
+    megItems.unshift({
+        description:`${textfieldEl.value}`,
+        owner:`${ownerNameEl.value}`,
+        price:`${priceEl.value}`,
+        contact:`${contactEl.value}`,
+        image: `${URL.createObjectURL(imageField.files[0])}`,
+        id:uuidv4()
+    })
     
+    console.log(megItems)
     console.log(`${inputValue} added to database`)
-     megsListEl.innerHTML += `<div class="megs">
-                                <img src="" id="pic" class="meg-img"/>
+
+    render()
+
+    textfieldEl.value = ''
+    priceEl.value = ""
+    ownerNameEl.value = ""
+    contactEl.value = ""
+}
+})  
+
+function getFeedHtml(){
+    let feedHtml = ``
+    megItems.forEach(function(item){
+         feedHtml += `<div class="megs">
+                                <img src="${item.image}" id="${item.id}" class="meg-img"/>
                                 <div class="meg-inputs">
-                                    <p>${inputValue}</p>
+                                    <p>${item.description}</p>
                                     <div class="sub-meg-inputs">
-                                        <h2>${priceEl.value}</h2>
+                                        <h2>${item.price}</h2>
                                         <div class="sub-final-meg-inputs">
-                                            <h2>${ownerNameEl.value}</h2>
-                                            <h3>${contactEl.value}</h3>
+                                            <h2>${item.owner}</h2>
+                                            <h3>${item.contact}</h3>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             `
-    inputValue = ""
-    priceEl.value = ""
-    ownerNameEl.value = ""
-    contactEl.value = ""
-})  
+    })
 
+    return feedHtml
+}
+
+function render(){
+    megsListEl.innerHTML = getFeedHtml()
+}

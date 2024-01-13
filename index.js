9b0cf73de5eaf75b0bd121da36b3694c17cbceba
DivvSaxena <saxenadivv@gmail.com>
2024-01-14 00:31:10 +0530

    add features

--- index.js
+++ index.js
@@ -16,10 +16,15 @@ let buttonEl = document.getElementById('button')
 
 let profilepic = document.getElementById('pic')
 let imageField = document.getElementById('image')
+let ownerNameEl = document.getElementById('owner-name')
+let priceEl = document.getElementById('price')
+let contactEl = document.getElementById('contact')
+let megsListEl = document.getElementById('megs-list')
 
 imageField.onchange = () => {
     profilepic.src = URL.createObjectURL(imageField.files[0])
 }
+
 
 buttonEl.addEventListener('click', () => {
     let inputValue = textfieldEl.value
@@ -27,4 +32,23 @@ buttonEl.addEventListener('click', () => {
     push(dataInDB, inputValue)
     
     console.log(`${inputValue} added to database`)
-})
+     megsListEl.innerHTML += `<div class="megs">
+                                <img src="" id="pic" class="meg-img"/>
+                                <div class="meg-inputs">
+                                    <p>${inputValue}</p>
+                                    <div class="sub-meg-inputs">
+                                        <h2>${priceEl.value}</h2>
+                                        <div class="sub-final-meg-inputs">
+                                            <h2>${ownerNameEl.value}</h2>
+                                            <h3>${contactEl.value}</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            `
+    inputValue = ""
+    priceEl.value = ""
+    ownerNameEl.value = ""
+    contactEl.value = ""
+})  
+
